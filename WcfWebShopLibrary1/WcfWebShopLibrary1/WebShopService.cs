a487b5e11c6037dcf4edf96eb28b2e1596447a6d
LAPTOP-HCLEN73H\lisev <lise.voorburg@student.hu.nl>
2018-05-30 17:04:29 +0200

    first commit

---
+++ WcfWebShopLibrary1/WcfWebShopLibrary1/WebShopService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfWebShopLibrary
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "WebShopService" in both code and config file together.
+    public class WebShopService : IWebShopService
+    {
+        public void PostNote(string from, string note)
+        {
+            Debug.WriteLine("{0}: {1}", from, note);
+        }
+
+    }
+}
---
+++ WcfWebShopLibrary1/WebShopHost/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using WcfWebShopLibrary;
+
+namespace WebShopHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(WebShopService)))
+            {
+                host.Open();
+                Console.WriteLine("Service ready");
+                Console.ReadKey();
+            }
+        }
+    }
+}
